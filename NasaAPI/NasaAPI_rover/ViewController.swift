c3734c18171e5d513186e69e8f674d34a8126e9e
batunpc <batuhnpc@gmail.com>
2022-08-09 01:39:03 -0400

    👾coredata

--- NasaAPI/NasaAPI_rover/ViewController.swift
+++ NasaAPI/NasaAPI_rover/ViewController.swift
@@ -6,13 +6,10 @@
 //
 import UIKit
 
-
-
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var tableView: UITableView!
+    
     lazy var resultSet = [Photo](){
         didSet{
             tableView.reloadData()
@@ -21,8 +18,7 @@ class ViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        // Do any additional setup after loading the view.        
         Service.getInfo(iUrl: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY") { result in
             
             DispatchQueue.main.async { [unowned self] in
@@ -30,35 +26,10 @@ class ViewController: UIViewController {
                 print(resultSet)
             }
         }
-        
-        
     }
-    
 }
-//
-//func performRequest(urlString: String, nasa_img:UIImage){
-//    //1- Create a URL
-//     guard let url = URL(string: urlString) else { return }
-//     //2- Use existing URLSession
-//     let session = URLSession.shared // (configuration: .default)
-//     //3- Give the session a task
-//    let task = session.dataTask(with: url) { data, response, error in
-//        if let error = error {
-//            print("error: \(error)" )
-//        }
-//        if let safeData = data {
-//            if var img = UIImage(data: safeData){
-//                DispatchQueue.main.async {
-//
-//                }
-//            }
-//        }
-//    }
-//     //4- Start the Task
-//     task.resume()
-//}
+
 extension ViewController : UITableViewDelegate, UITableViewDataSource{
-    
  
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -72,34 +43,21 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource{
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as!
             RoverImagesTableViewCell
         cell.dateLbl.text = resultSet[indexPath.row].earth_date
-        //cell.rImage.image = UIImage(named: resultSet[indexPath.row].img_src)
         
         let urlString = resultSet[indexPath.row].img_src
         
         if let url = URL(string: urlString) {
-            URLSession.shared.dataTask(with: url) { (data, urlResponse, error) in
+            let task = URLSession.shared.dataTask(with: url) { (data, urlResponse, error) in
                     if let data = data {
                         DispatchQueue.main.async {
                             cell.rImage.image = UIImage(data: data)
-                        }
                     }
-            }.resume()
-            
-//        if let nasa_img_url = URL(string: resultSet[indexPath.row].img_src){
-//    //    print(nasa_img_url)
-//
-//            do{
-//                let imgData = try Data(contentsOf: nasa_img_url)
-//                cell.rImage.image = UIImage(data: imgData)
-//            }
-//            catch{
-//                print("Error occured \(error)")
-//            }
-//
-//        }
-   
-        
+                }
+            }
+            task.resume()
+    
+        }
+    return cell
     }
-        return cell
-}
+
 }
